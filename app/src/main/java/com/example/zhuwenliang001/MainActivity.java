6a91ab10ecd62e78cc0fa1d0d27c64c6407f6dfa
朱文亮 <592277244@qq.com>
2020-08-31 11:55:17 +0800

    第二次提交

--- app/src/main/java/com/example/zhuwenliang001/MainActivity.java
+++ app/src/main/java/com/example/zhuwenliang001/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.zhuwenliang001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hahahah");
+        System.out.println("第二次提交");
+        Toast.makeText(this,"hehe",Toast.LENGTH_LONG).show();
     }
 }
